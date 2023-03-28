b93deefbe5c06355505f6839ea3017593865ef3c
alanjslattery <alan.slattery@gmail.com>
2023-03-28 09:50:15 +0100

    BlueParrott SDK v4.6.00
    Included in zip :
    - blueparrottsdk-release  - .aar file to import into your Android Studio project
    - blueparrottsdkdemo - basic demo that shows how to connect to sdk, enable the sdk, listen for button events and update config settings on headset
    - javadocs  - java reference documentation
    
    New in 4.6.00
    - Deprecated AT-only version (limited SDK)
    - Workaround to deal with bug in Android OS which blocks handling of Bluetooth commands on Work Profile
    - Workaround for Bug in Samsung Work Profile implementation which results in duplicate events
    - New SDK Methods getModel, getMacAddress, getFriendlyName
    - New method setSendAnalytics and getSendAnalytics for sending mode customisation analytics
    - java docs update for new methods
    - sdkdemoapp updated to use new methods

---
+++ BP_SDK_ANDROID_4.6.00/javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.blueparrott.blueparrottsdk","l":"BPHeadset"},{"p":"com.blueparrott.blueparrottsdk","l":"BPHeadsetListener"},{"p":"com.blueparrott.blueparrottsdk","l":"BPSdk"},{"p":"com.blueparrott.blueparrottsdk","l":"IBPHeadsetListener"}]
